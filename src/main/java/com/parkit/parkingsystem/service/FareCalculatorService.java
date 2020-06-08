101e8ced8cb2545d6acf3c649dd10a6ceab3cf3a
estated90 <50859250+estated90@users.noreply.github.com>
2020-06-08 19:59:21 +0200

    Correction of the date format
    Change of all new date to LocalDateTime
    Add the conversion in sql for the set
    Add the Conversion to LocalDateTime for the get from SQL
    Correction of the bugs

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,28 +1,34 @@
 package com.parkit.parkingsystem.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
+        if( (ticket.getOutTime() == null) || (ticket.getOutTime().isBefore(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        LocalDateTime inHour = ticket.getInTime();
+        LocalDateTime outHour = ticket.getOutTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = ChronoUnit.MINUTES.between(inHour, outHour);
+        double fareCarPerMinute = (Fare.CAR_RATE_PER_HOUR / 60);
+        double fareBikePerMinute = (Fare.BIKE_RATE_PER_HOUR / 60);
+
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(duration * fareCarPerMinute);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(duration * fareBikePerMinute);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
