718d8e59889951dc70b70bc20e97b8a4c5501a0e
estated90 <50859250+estated90@users.noreply.github.com>
2020-06-10 15:20:15 +0200

    Correction in the tests

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -21,54 +21,51 @@ public class FareCalculatorService {
 
 	public void calculateFare(Ticket ticket, String vehicleRegNumber) {
 		Connection con = null;
-
+		boolean hasNext = false;
 		if ((ticket.getOutTime() == null) || (ticket.getOutTime().isBefore(ticket.getInTime()))) {
 			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
 		}
+		LocalDateTime inHour = ticket.getInTime();
+		LocalDateTime outHour = ticket.getOutTime();
+		double duration = ChronoUnit.MINUTES.between(inHour, outHour);
 		try {
-			LocalDateTime inHour = ticket.getInTime();
-			LocalDateTime outHour = ticket.getOutTime();
-			double duration = ChronoUnit.MINUTES.between(inHour, outHour);
-			double fareCarPerMinute = 0;
-			double fareBikePerMinute = 0;
-
 			con = dataBaseConfig.getConnection();
 			PreparedStatement ps = con.prepareStatement(DBConstants.GET_EXISTING_VEHICLE);
 			ps.setString(1, vehicleRegNumber);
 			ResultSet rs = ps.executeQuery();
-			boolean hasNext = rs.next();
-
-			fareCarPerMinute = (Fare.CAR_RATE_PER_HOUR / 60);
-			fareBikePerMinute = (Fare.BIKE_RATE_PER_HOUR / 60);
-
-			if (duration <= 30) {
-				ticket.setPrice(0);
-			} else {
-				switch (ticket.getParkingSpot().getParkingType()) {
-				case CAR: {
-					if (hasNext) {
-						ticket.setPrice(duration * fareCarPerMinute * 0.95);
-					} else {
-						ticket.setPrice(duration * fareCarPerMinute);
-					}
-					break;
-				}
-				case BIKE: {
-					if (hasNext) {
-						ticket.setPrice(duration * fareBikePerMinute * 0.95);
-					} else {
-						ticket.setPrice(duration * fareBikePerMinute);
-					}
-					break;
-				}
-				default:
-					throw new IllegalArgumentException("Unkown Parking Type");
-				}
-			}
+			hasNext = rs.next();
 		} catch (Exception ex) {
 			logger.error("Error fetching if historic exist", ex);
 		} finally {
 			dataBaseConfig.closeConnection(con);
 		}
+
+		if (duration <= 30) {
+			ticket.setPrice(0);
+		} else {
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				double fareCarPerMinute = (Fare.CAR_RATE_PER_HOUR / 60);
+				if (hasNext) {
+					ticket.setPrice(duration * fareCarPerMinute * 0.95);
+				} else {
+					ticket.setPrice(duration * fareCarPerMinute);
+				}
+				break;
+			}
+			case BIKE: {
+				double fareBikePerMinute = (Fare.BIKE_RATE_PER_HOUR / 60);
+				if (hasNext) {
+					ticket.setPrice(duration * fareBikePerMinute * 0.95);
+				} else {
+					ticket.setPrice(duration * fareBikePerMinute);
+				}
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
+		}
+
 	}
 }
